3a429561732c003f12683d8b760e07780ddb6bd2
Mirlanbeck <mail_to_mirlan@mail.ru>
2025-09-06 16:12:25 +0600

    feat: tasks solved

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -108,7 +108,9 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(x1, y1, x2, y2) {}
+function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
+  throw new Error('Not implemented');
+}
 
 /**
  * Returns a last digit of a integer number.
