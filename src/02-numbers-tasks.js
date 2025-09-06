600e4b7c48780f8038190d63d634f906ffa500d8
Mirlanbeck <mail_to_mirlan@mail.ru>
2025-09-06 16:37:25 +0600

    feat: updated

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -205,7 +205,7 @@ function roundToPowerOfTen(num, pow) {
 function isPrime(n) {
   if (n <= 1) return false;
 
-  for (let i = 2; i <= Math.sqrt(n); i++) {
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
     if (n % i === 0) return false;
   }
 
